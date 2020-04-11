ee4bd1ba14bf19ff23e0480141a11f21cc4bb036
Andras Niedermayer <afniedermayer@gmail.com>
2020-04-11 17:42:47 +0200

    adjusted split_latex_frame to use latex_frame_split code and adjusted UI

--- split_latex_frame.py
+++ split_latex_frame.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 from tkinter import ttk
 import sys
 import traceback
+from latex_frame_split import get_environment, split_frame
 
 def items_before(original_latex:str, split:int) -> str:
     r"""returns a frame that contains items 1 to split
@@ -71,44 +72,50 @@ class Application(ttk.Frame):
         opts = {"padx": 20, "pady": 5}
         ttk.Label(self, text="Input Text").grid(row=0, column=0, columnspan=2,
                  sticky=tk.W, **opts)
-        self.clear_button = ttk.Button(self, text="Clear", command=self.clear_input)
+        self.clear_button = ttk.Button(self, text="Clear", command=self.clear_input, underline=0)
         self.clear_button.grid(row=0, column=2, sticky=tk.E, **opts)
         self.input_text = scrolledtext.ScrolledText(self, height=8)
         self.input_text.grid(row=1, column=0, columnspan=3, **opts)
-        ttk.Label(self, text="Insert after item number:").grid(row=2, column=0,
-                 sticky=tk.NW, **opts)
-        self.split_after = ttk.Entry(self)
-        self.split_after.grid(row=2, column=2, sticky=tk.W+tk.E, **opts)
-        self.split_button = ttk.Button(self,
-            text="Split text\nand copy to >>\nclipboard", command=self.split_text)
+        # ttk.Label(self, text="Insert after item number:").grid(row=2, column=0,
+        #          sticky=tk.NW, **opts)
+        # self.split_after = ttk.Entry(self)
+        # self.split_after.grid(row=2, column=2, sticky=tk.W+tk.E, **opts)
+        self.split_button = ttk.Button(self, text="Split text and copy to clipboard", 
+            command=self.split_text, underline=0)
         self.split_button.grid(row=3, column=1)
         ttk.Label(self, text="Output Text").grid(row=4, column=0, columnspan=3,
                  sticky=tk.W, **opts)
         self.output_text = scrolledtext.ScrolledText(self, height=8)
         self.output_text.grid(row=5, column=0, columnspan=3, pady=(5,20), padx=20)
+        self.master.bind('<Alt-s>', lambda event: self.split_text())
+        self.master.bind('<Alt-c>', lambda event: self.clear_input())
 
     def split_text(self):
+        position = self.input_text.count('1.0', tk.INSERT)[0]
+        buffer = self.input_text.get("1.0", "end-1c")
+        self.output_text.delete("1.0", "end")
+
         try:
-            try:
-                split = int(self.split_after.get())
-            except ValueError:
-                messagebox.showerror("Error", "'{}' is not a valid item number".format(self.split_after.get()))
-                return
-            input_string = self.input_text.get("1.0", "end-1c")
-            self.output_text.delete("1.0", "end")
-            output_string = items_before(input_string, split) + '\n' + items_after(input_string, split)
-            self.output_text.insert("1.0", output_string)
-            self.master.clipboard_clear()
-            self.master.clipboard_append(output_string)
-            self.output_text.focus_set()
-            self.output_text.tag_add("sel", "1.0", "end")
-        except Exception as e:
+            frame, frame1, frame2 = split_frame(buffer, position)
+        except ValueError as e:
             traceback.print_exc()
-            messagebox.showerror("Error", "Error occurred during split: {}".format(e))
+            self.show_warning('Error: {}'.format(e.args[0]))
+            return
+        new_frame = frame1 + '\n\n' + frame2
+
+        self.output_text.insert("1.0", new_frame)
+        self.master.clipboard_clear()
+        self.master.clipboard_append(new_frame)
+        self.output_text.focus_set()
+        self.output_text.tag_add("sel", "1.0", "end")
 
     def clear_input(self):
         self.input_text.delete("1.0", "end")
         self.input_text.focus_set()
+
+    def show_warning(self, message):
+        messagebox.showerror("Error", message)
+
 
 def main():
     try:
